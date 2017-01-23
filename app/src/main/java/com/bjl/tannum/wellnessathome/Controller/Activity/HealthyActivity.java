a658a25baa8dc3f2e301e1cfadac66df6184f1f6
tannum <tannummail@gmail.com>
2017-01-24 05:44:38 +0700

    24/01/17

--- app/src/main/java/com/bjl/tannum/wellnessathome/Controller/Activity/HealthyActivity.java
+++ app/src/main/java/com/bjl/tannum/wellnessathome/Controller/Activity/HealthyActivity.java
@@ -4,59 +4,113 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Camera;
+//import android.graphics.Camera;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.os.EnvironmentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bjl.tannum.wellnessathome.R;
 
-public class HealthyActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.File;
+import java.util.List;
+
+public class HealthyActivity extends AppCompatActivity implements SurfaceHolder.Callback{
 
     static final int MY_PERMISSIONS_ACCESS_CAMERA = 0;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView result;
     Button button;
+    private Uri imageUri;
+
+
+    //Camera mCamera;
+
+    Camera mCamera;
+    SurfaceView mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_healthy);
 
-        button = (Button)findViewById(R.id.camera);
-        button.setOnClickListener(this);
-        result = (ImageView)findViewById(R.id.imageView);
-          CheckCameraPermission();
+//        button = (Button)findViewById(R.id.camera);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        result = (ImageView)findViewById(R.id.imageView);
+//          CheckCameraPermission();
+
+
+
+
+
+
+
+        //CheckCameraPermission();
+        mPreview = (SurfaceView)findViewById(R.id.preview);
+        mPreview.getHolder().addCallback(this);
+        mPreview.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+
 
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+    public void onResume() {
+        //Log.d("System","onResume");
+        super.onResume();
+        mCamera = Camera.open();
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            result.setImageBitmap(imageBitmap);
-        }
+    public void onPause() {
+       // Log.d("System","onPause");
+        super.onPause();
+        mCamera.release();
     }
 
 
-    @Override
-    public void onClick(View view) {
-        dispatchTakePictureIntent();
-    }
+
+    //    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+//
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            result.setImageBitmap(imageBitmap);
+//        }
+//
+//
+//    }
+
+
+
 
 
     private void CheckCameraPermission() {
@@ -68,5 +122,38 @@ public class HealthyActivity extends AppCompatActivity implements View.OnClickLi
                     new String[]{Manifest.permission.CAMERA},
                     MY_PERMISSIONS_ACCESS_CAMERA);
         }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try{
+            mCamera.setPreviewDisplay(mPreview.getHolder());
+        }catch (Exception e){
+            Log.d("debug",e.toString());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.d("debug","surfaceChanged");
+        Camera.Parameters params = mCamera.getParameters();
+        List<Camera.Size> previewSize = params.getSupportedPreviewSizes();
+        List<Camera.Size> pictureSize = params.getSupportedPictureSizes();
+        params.setPictureSize(pictureSize.get(0).width,pictureSize.get(0).height);
+        params.setPreviewSize(previewSize.get(0).width,previewSize.get(0).height);
+        params.setJpegQuality(100);
+        mCamera.setParameters(params);
+
+        try {
+            mCamera.setPreviewDisplay(mPreview.getHolder());
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
